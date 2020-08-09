68dc1e99a360a55c49f2df246d331434976b607a
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-08-09 16:20:21 -0700

    Added Signin logic

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -8,6 +8,7 @@ const authReducer = (state, action) => {
         case 'add_error':
             return { ...state, errorMessage:action.payload };
         case 'signup':
+        case 'signin':
             return { errorMessage: '', token:action.payload };
         default:
             return state;
@@ -26,10 +27,18 @@ const signup = dispatch => async ({ email, password }) => {
     }
 };
 
-const signin = (dispatch) => {
-    return ({ email, password }) => {
-    
-    };
+const signin = dispatch => async ({ email, password }) => {
+    try {
+        const response = await trackerApi.post('/signin', { email, password });
+        await AsyncStorage.setItem('token', response.data.token);
+        dispatch({ type: 'signin', payload: response.data.token });
+        navigate('TrackList');
+    } catch (err) {
+        dispatch({
+            type: 'add_error',
+            payload: 'Something went wrong with sign in'
+        });
+    }
 };
 
 const signout = (dispatch) => {
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SigninScreen = () => {
-    return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+    const { state, signin } = useContext(AuthContext);
+    return (
+        <View style={styles.container}>
+            <AuthForm
+                headerText="Sign In to your Account"
+                errorMessage={state.errorMessage}
+                onSubmit={signin}
+                submitButtonText="Sign In"
+            />
+            <NavLink 
+                text="Don't have an account? Sign up instead" 
+                routeName="Signup"
+            />
+        </View>
+    );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = {
+    header: () => false
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 150
+    }
+});
 
 export default SigninScreen;
