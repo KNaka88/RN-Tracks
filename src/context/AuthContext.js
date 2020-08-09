9702187f6b0fad549d6e979844e36448d1e4badd
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-08-09 16:45:19 -0700

    Clear error messages and make login loading nicer

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,9 +10,25 @@ const authReducer = (state, action) => {
         case 'signup':
         case 'signin':
             return { errorMessage: '', token:action.payload };
+        case 'clear_error_message':
+            return { ...state, errorMessage: '' };
         default:
             return state;
     }
+};
+
+const clearErrorMessage = dispatch => () => {
+    dispatch({ type: 'clear_error_message'});
+};
+
+const tryLocalSignin = dispatch => async () => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+      dispatch({ type: 'signin', payload: token });
+      navigate('TrackList');
+  } else {
+      navigate('Signup');
+  }
 };
 
 const signup = dispatch => async ({ email, password }) => {
@@ -51,6 +67,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
     authReducer,
-    { signin, signout, signup },
+    { signin, signout, signup, clearErrorMessage, tryLocalSignin },
     { token: null, errorMessage: '' }
 );
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,13 +1,17 @@
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { View, StyleSheet } from 'react-native';
 import { Context as AuthContext } from '../context/AuthContext';
 
 const SigninScreen = () => {
-    const { state, signin } = useContext(AuthContext);
+    const { state, signin, clearErrorMessage } = useContext(AuthContext);
     return (
         <View style={styles.container}>
+            <NavigationEvents
+                onWillBlur={clearErrorMessage}
+            />
             <AuthForm
                 headerText="Sign In to your Account"
                 errorMessage={state.errorMessage}
