d45928d0641713f590cc7b64ff92b869d125e866
Koji Nakagawa <lightupthesky0627@gmail.com>
2020-08-19 23:19:56 -0700

    Disable location when not using

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from 'react';
 import { Accuracy, requestPermissionsAsync, watchPositionAsync} from 'expo-location';
 
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
     const [err, setErr] = useState(null);
+    const [subscriber, setSubscriber] = useState(null);
 
     const startWatching = async () => {
         try {
@@ -11,21 +12,27 @@ export default (callback) => {
             if (!granted) {
                 throw new Error('Location permission not granted');
             }
-            await watchPositionAsync({
+            const sub = await watchPositionAsync({
                 accuracy: Accuracy.BestForNavigation,
                 timeInterval: 1000,
                 distanceInterval: 10
             },
             callback
             );
+            setSubscriber(sub);
         } catch (e) {
             setErr(e);
         }
     };
 
     useEffect(() => {
-        startWatching();
-    }, []);
+        if (shouldTrack) {
+            startWatching();            
+        } else {
+            subscriber.remove();
+            setSubscriber(null);
+        }
+    }, [shouldTrack]);
 
     return [err];
 };
